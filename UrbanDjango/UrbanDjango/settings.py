71729302feded104b99968bf5969130539633ee6
Home <alex.gerasimov@bk.ru>
2025-02-03 21:58:08 +0700

    "Настраиваем СУБД postgre в django"

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -74,13 +74,29 @@ WSGI_APPLICATION = 'UrbanDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Test1',
+        'USER': 'postgres',
+        'PASSWORD': 'postgre',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
+# >>> from task1.models import Game
+# >>> Game.objects.count()
+# 0
+# >>> from task1.models import Buyer
+# >>> Buyer.objects.all()
+# <QuerySet [<Buyer: Buyer object (1)>, <Buyer: Buyer object (2)>, <Buyer: Buyer object (3)>]>
+# >>>
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
